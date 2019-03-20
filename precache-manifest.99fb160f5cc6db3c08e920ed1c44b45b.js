d8593086f45ecf329033daa8937f768b576f52bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-20 01:31:33 +0000

    Deploy brion25/turing-exercise to github.com/brion25/turing-exercise.git:gh-pages

---
+++ precache-manifest.99fb160f5cc6db3c08e920ed1c44b45b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd07492c271740a09458",
+    "url": "/turing-exercise/static/css/main.32e19776.chunk.css"
+  },
+  {
+    "revision": "bd07492c271740a09458",
+    "url": "/turing-exercise/static/js/main.fe0f39ba.chunk.js"
+  },
+  {
+    "revision": "1898b0bf57bc4b78932a",
+    "url": "/turing-exercise/static/js/runtime~main.96d5a281.js"
+  },
+  {
+    "revision": "e3d654986620b642f46c",
+    "url": "/turing-exercise/static/js/2.6cbda4fe.chunk.js"
+  },
+  {
+    "revision": "3823908019fd5e4edee6967611b64fe0",
+    "url": "/turing-exercise/static/media/department-seasonal.38239080.jpg"
+  },
+  {
+    "revision": "983854ab33e353f456635f5189ca82e0",
+    "url": "/turing-exercise/static/media/nature.983854ab.jpg"
+  },
+  {
+    "revision": "144da9146f0ea2fed6420346a5f54a70",
+    "url": "/turing-exercise/static/media/regional.144da914.jpg"
+  },
+  {
+    "revision": "0b1392f613686903cbce2c68c3c7e86b",
+    "url": "/turing-exercise/static/media/register.0b1392f6.jpg"
+  },
+  {
+    "revision": "15b105d8fc22bb93ca7be13a8b207c1e",
+    "url": "/turing-exercise/index.html"
+  }
+];
